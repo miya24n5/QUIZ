6458e4817a1f204c861da73f733f7cd43738b9e3
miya24n5 <sho.sakulai10@gmail.com>
2022-04-14 11:05:41 +0900

    javaで答えを選択したときの動きを追加

--- questions.js
+++ questions.js
@@ -1,21 +1,70 @@
-const bmi = calculateBmi(1.61, 56);
+// BMIを知れるif文をお試しで作成
+// const bmi = calculateBmi(1.61, 56);
 
-let message = 'わかりません';
+// let message = 'わかりません';
+
+// if (bmi < 18.5) {
+//   message = '低体重です';
+// } else if (bmi >= 25) {
+//   message = '太っています';
+// } else {
+//   message = '普通です';
+// }
+
+// console.log(message);
+
+// function calculateBmi(height, weight) {
+//     // BMI = 体重計[kg]　/　(身長[m] ** 2)
+//     return weight / (height ** 2);
+// }
+
+const answersList = document.querySelectorAll('ol.answers li');
 
 
-if (bmi < 18.5) {
-  message = '低体重です';
-} else if (bmi >= 25) {
-  message = '太っています';
-} else {
-  message = '普通です';
+answersList.forEach(li => li.addEventListener('click', checkClickedAnswer))
+// 正しい答え
+const correctAnswers = {
+  question1: 'D',
+  question2: 'C',
+  question3: 'A',
+  question4: 'C',
 }
+function checkClickedAnswer(event){
+  // クリックされた答えの要素（liタグ）
+  const clickedAnswerElement = event.currentTarget;
+  console.log(clickedAnswerElement.dataset.answer);
 
-console.log(message);
+  // 選択した答え(A,B,C,D)
+  const selectedAnswer = clickedAnswerElement.dataset.answer;
 
-function calculateBmi(height, weight) {
-    // BMI = 体重計[kg]　/　(身長[m] ** 2)
-    return weight / (height ** 2);
+  const questionId = clickedAnswerElement.closest('ol.answers').dataset.id;
+  // 正しい答え(A,B,C,D)
+  const correctAnswer = correctAnswers[questionId];
+
+  // メッセージを入れる変数を用意
+  let message;
+
+  // カラーコードを入れる変数を用意
+  let answerColorCode;
+
+  // 答えが正しいか判定
+  if (selectedAnswer === correctAnswer) {
+    // 正しい答えだった時
+    message = '正解です！おめでとう！';
+    answerColorCode = '';
+  } else {
+    // 間違った答えだった時
+    message = 'ざんねん！不正解です！';
+    answerColorCode = '#f05959';
+  }
+  
 
 
+  alert(message);
+
+  // 色を変更(間違っていた時だけ色が変わる)
+  document.querySelector('span#correct-answer').style.color = answerColorCode;
+
+  // 答え全体を表示
+  document.querySelector('div#section-correct-answer').style.display = 'block';
 }
